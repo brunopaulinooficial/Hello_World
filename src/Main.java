2b8a136874d099bd77b770955ffa90aad65c9d70
bruno <brunopaulinoc@hotmail.com>
2023-05-12 20:02:02 -0300

    Primeiro Código

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Meu primeiro código.
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("Olá, seja bem vindo!");
+        System.out.println("This is my first code in JAVA.");
+        System.out.print("Esse é o meu primeiro código em JAVA.");
+
+
+    }
+}
